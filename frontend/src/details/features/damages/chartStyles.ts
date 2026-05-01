c238cc2e1b0d162cafa1535ec32235f6e7f4b0c9
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-05-01 16:56:23 +0100

    refactor: RCP chart colours (#455)
    Add a reference for the RCP chart colours.

--- frontend/src/details/features/damages/chartStyles.ts
+++ frontend/src/details/features/damages/chartStyles.ts
@@ -4,6 +4,7 @@ export const chartStyles = {
     type: 'ordinal',
     scale: {
       domain: ['baseline', '2.6', '4.5', '8.5'],
+      // Drawn from IPCC AR6 colormap https://pyam-iamc.readthedocs.io/en/stable/tutorials/ipcc_colors.html
       range: ['#8b8b8b', '#003466', '#709fcc', '#980002'],
     },
     title: 'RCP',
