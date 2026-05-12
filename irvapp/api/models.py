b95084baf3fe7061d8d8fa8a9155427cbe9a35ca
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-05-12 17:54:15 +0100

    chore: use postgis backend
    Update the Django app to use  PostGIS as the database backend, via GeoDjango.

--- irvapp/api/models.py
+++ irvapp/api/models.py
@@ -5,7 +5,7 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
+from django.contrib.gis.db import models
 
 
 class AdaptationCostBenefit(models.Model):
@@ -101,20 +101,8 @@ class Features(models.Model):
     layer = models.ForeignKey(FeatureLayers, models.DO_NOTHING, db_column='layer')
     sublayer = models.CharField(blank=True, null=True)
     properties = models.TextField()  # This field type is a guess.
-    geom = models.TextField()  # This field type is a guess.
+    geom = models.GeometryField()
 
     class Meta:
         managed = False
         db_table = 'features'
-
-
-class SpatialRefSys(models.Model):
-    srid = models.IntegerField(primary_key=True)
-    auth_name = models.CharField(max_length=256, blank=True, null=True)
-    auth_srid = models.IntegerField(blank=True, null=True)
-    srtext = models.CharField(max_length=2048, blank=True, null=True)
-    proj4text = models.CharField(max_length=2048, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'spatial_ref_sys'
