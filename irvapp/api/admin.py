1c8a31e1d36affaabc314ff91951b3c3e66d38db
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-05-14 11:44:54 +0100

    chore: initial migration (#4)
    Add the API app to settings and create migrations for the initial models.

--- irvapp/api/admin.py
+++ irvapp/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import (
+    FeatureLayers,
+    Features,
+)
+
 # Register your models here.
+admin.site.register(Features)
+admin.site.register(FeatureLayers)
--- irvapp/irvapp/settings.py
+++ irvapp/irvapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'api',
 ]
 
 MIDDLEWARE = [
