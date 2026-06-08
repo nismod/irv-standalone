89d304e822af27c122c722742863c5f8ec1a4b94
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-06-08 17:36:09 +0100

    refactor: async network tree state (#46)
    Move network tree hierarchy state to an atom that can be asynchronously initialized from URL/session storage.

--- frontend/src/data-layers/networks/state/data-selection.ts
+++ frontend/src/data-layers/networks/state/data-selection.ts
@@ -13,60 +13,75 @@ import { NETWORK_LAYERS_HIERARCHY } from '../sidebar/hierarchy';
 
 export const networkTreeExpandedState = atom<string[]>([]);
 
-export const networkTreeConfig = buildTreeConfig(NETWORK_LAYERS_HIERARCHY);
-const networkTreeURLs = mapValues(networkTreeConfig.nodes, (node) => node.url);
-const networkTreeIDs = Object.keys(networkTreeURLs);
+export const networkTreeHierarchyState = atom(NETWORK_LAYERS_HIERARCHY);
+export const networkTreeConfigState = atom((get) => {
+  const networkTreeHierarchy = get(networkTreeHierarchyState);
+  return buildTreeConfig(networkTreeHierarchy);
+});
+const networkTreeUrlState = atom((get) => {
+  const networkTreeConfig = get(networkTreeConfigState);
+  return mapValues(networkTreeConfig.nodes, node => node.url);
+});
+const networkTreeIdState = atom((get) => {
+  const networkTreeURLs = get(networkTreeUrlState);
+  return Object.keys(networkTreeURLs);
+});
 
-function parseTreeFromString(value: string) {
-  const separator = value.includes('.') ? '.' : ',';
-  const checkedFields = value.split(separator).filter(Boolean);
-  const checked = {};
-  checkedFields.forEach((url) => {
-    const id = networkTreeIDs.includes(url)
-      ? url // url is a layer ID.
-      : networkTreeIDs.find((id) => networkTreeURLs[id] === url); // url is the hex code for a layer.
-    if (id !== undefined) {
-      checked[id] = true;
-    }
-  });
-  return recalculateCheckboxStates({ checked, indeterminate: {} }, networkTreeConfig);
-}
+const defaultNetworkTreeState = atom<CheckboxTreeState>((get) => {
+  const networkTreeConfig = get(networkTreeConfigState);
+  return {
+    checked: mapValues(networkTreeConfig.nodes, () => false),
+    indeterminate: mapValues(networkTreeConfig.nodes, () => false),
+  };
+});
 
-function stringifyTree(tree: CheckboxTreeState) {
-  const checkedLayers = Object.keys(tree.checked).filter(
-    (id) =>
-      tree.checked[id] && networkTreeConfig.nodes[id] && !networkTreeConfig.nodes[id].children,
-  );
-  const checked = checkedLayers.map((id) => networkTreeURLs[id]);
-  return checked.join('.');
-}
-
-const defaultNetworkTreeState: CheckboxTreeState = {
-  checked: mapValues(networkTreeConfig.nodes, () => false),
-  indeterminate: mapValues(networkTreeConfig.nodes, () => false),
-};
-
-const _networkTreeBase = atom<CheckboxTreeState>(
-  (() => {
-    const params = new URLSearchParams(window.location.search);
-    const raw = params.get('netTree') ?? sessionStorage.getItem(STORAGE_PREFIX + 'netTree');
-    if (!raw) return defaultNetworkTreeState;
-    return parseTreeFromString(raw);
-  })(),
-);
+const _networkTreeBase = atom(null as CheckboxTreeState | null); 
 
 export const networkTreeCheckboxState = atom(
-  (get) => get(_networkTreeBase),
-  (_get, set, newTree: CheckboxTreeState) => {
+  (get) => {
+    function parseTreeFromString(value: string) {
+      const networkTreeConfig = get(networkTreeConfigState);
+      const networkTreeURLs = get(networkTreeUrlState);
+      const networkTreeIDs = get(networkTreeIdState);
+      const separator = value.includes('.') ? '.' : ',';
+      const checkedFields = value.split(separator).filter(Boolean);
+      const checked: Record<string, boolean> = {};
+      checkedFields.forEach((url) => {
+        const id = networkTreeIDs.includes(url)
+          ? url // url is a layer ID.
+          : networkTreeIDs.find((id) => networkTreeURLs[id] === url); // url is the hex code for a layer.
+        if (id !== undefined) {
+          checked[id] = true;
+        }
+      });
+      return recalculateCheckboxStates({ checked, indeterminate: {} }, networkTreeConfig);
+    }
+
+    const networkTree = get(_networkTreeBase);
+    const defaultNetworkTree = networkTree || get(defaultNetworkTreeState);
+    const params = new URLSearchParams(window.location.search);
+    const raw = params.get('netTree') ?? sessionStorage.getItem(STORAGE_PREFIX + 'netTree');
+    if (!raw) return defaultNetworkTree;
+    return parseTreeFromString(raw);
+  },
+  (get, set, newTree: CheckboxTreeState) => {
+    const networkTreeConfig = get(networkTreeConfigState);
+    const networkTreeURLs = get(networkTreeUrlState);
     set(_networkTreeBase, newTree);
-    const str = stringifyTree(newTree);
+    const checkedLayers = Object.keys(newTree.checked).filter(
+      (id) =>
+        newTree.checked[id] && networkTreeConfig.nodes[id] && !networkTreeConfig.nodes[id].children,
+    );
+    const checked = checkedLayers.map((id) => networkTreeURLs[id]);
+    const str = checked.join('.');
     sessionStorage.setItem(STORAGE_PREFIX + 'netTree', str);
     set(atomWithQueryParams, setUrlParam('netTree', str));
-  },
+  }
 );
 
 export const networkSelectionState = atom<string[]>((get) => {
   const checkboxState = get(networkTreeCheckboxState);
+  const networkTreeConfig = get(networkTreeConfigState);
 
   return Object.keys(checkboxState.checked).filter(
     (id) =>
