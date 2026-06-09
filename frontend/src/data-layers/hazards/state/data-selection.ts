1efcd16dd517ffe55fddb584fd72b5e33d521170
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-06-09 15:58:16 +0100

    refactor: hazard domain state (#49)
    Move hazard domain state into an atom, in preparation for reading it from the backend.

--- frontend/src/data-layers/hazards/state/data-selection.ts
+++ frontend/src/data-layers/hazards/state/data-selection.ts
@@ -1,8 +1,12 @@
-import { Atom } from 'jotai';
+import { atom, type Atom } from 'jotai';
 import { atomFamily } from 'jotai-family';
 import fromPairs from 'lodash/fromPairs';
 
 import { atomWithStoredBool } from 'lib/state/map-view/map-url';
+import { HAZARD_DOMAINS } from 'app/config/sidebar/HAZARD_DOMAINS';
+
+export type HazardDomains = typeof HAZARD_DOMAINS;
+export const hazardDomainState = atom(HAZARD_DOMAINS);
 
 export const hazardSelectionState = atomFamily((id: string) => atomWithStoredBool(id, false));
 
