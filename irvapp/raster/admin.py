4ccb06e23e41e5eb4b5b94ff3f23f62c52a87b89
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-07-16 13:52:18 +0100

    refactor: raster tile source admin (#91)
    Table view fields for raster tile source admin.

--- irvapp/raster/admin.py
+++ irvapp/raster/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import RasterTileSource
 
-admin.site.register(RasterTileSource)
+
+@admin.register(RasterTileSource)
+class RasterTileSourceAdmin(admin.ModelAdmin):
+    list_display = ["name", "domain", "group", "license", "keys"]
