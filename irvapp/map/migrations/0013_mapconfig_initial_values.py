7aa2ee8eaf13a13dd9ba897fd4f15f4b1298f763
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-07-21 16:37:59 +0100

    feat: initial config
    Seed the `MapConfig` model with initial values for the map name, bounds and zoom.

---
+++ irvapp/map/migrations/0013_mapconfig_initial_values.py
@@ -0,0 +1,42 @@
+from django.db import migrations
+
+
+INITIAL_MAP_CONFIG = (
+    ("appName", "J-SRAT", "string"),
+    ("latitude", "18.14", "number"),
+    ("longitude", "-77.28", "number"),
+    ("zoom", "9", "number"),
+    ("minZoom", "3", "number"),
+    ("maxZoom", "16", "number"),
+    ("pitch", "0", "number"),
+)
+
+
+def seed_map_config(apps, schema_editor):
+    MapConfig = apps.get_model("map", "MapConfig")
+
+    for config_name, config_value, config_type in INITIAL_MAP_CONFIG:
+        MapConfig.objects.update_or_create(
+            config_name=config_name,
+            defaults={
+                "config_value": config_value,
+                "config_type": config_type,
+            },
+        )
+
+
+def unseed_map_config(apps, schema_editor):
+    MapConfig = apps.get_model("map", "MapConfig")
+    config_names = [config_name for config_name, _, _ in INITIAL_MAP_CONFIG]
+    MapConfig.objects.filter(config_name__in=config_names).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("map", "0012_dataset_quantity"),
+    ]
+
+    operations = [
+        migrations.RunPython(seed_map_config, unseed_map_config),
+    ]
