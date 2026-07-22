427a07fb4b994de24efb573f316f0bad15fccbda
Jim O'Donnell <james.odonnell@dtc.ox.ac.uk>
2026-07-22 10:57:43 +0100

    fix: dataset access permission checks
    Fix a bug where the `/tiles/raster/sources/[dataset_id]/domains` endpoint fails with:
    ```
    AttributeError: 'dict' object has no attribute 'access_groups'
    ```

--- irvapp/map/permissions.py
+++ irvapp/map/permissions.py
@@ -1,7 +1,24 @@
 from rest_framework.permissions import BasePermission
 
 
+def _coerce_dataset_instance(dataset):
+    if not isinstance(dataset, dict):
+        return dataset
+
+    dataset_id = dataset.get("id")
+    if dataset_id is None:
+        return None
+
+    from .models import Dataset
+
+    return Dataset.objects.prefetch_related("access_groups").filter(
+        pk=dataset_id
+    ).first()
+
+
 def user_has_dataset_access(user, dataset, user_group_ids=None):
+    dataset = _coerce_dataset_instance(dataset)
+
     if dataset is None or not user.is_authenticated:
         return False
     if user.is_superuser:
