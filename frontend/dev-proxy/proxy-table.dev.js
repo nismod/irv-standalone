f7ba36e6b7c84fd25b6f0f40f99eb0381b2e03d5
Fred Thomas <fred.thomas@eci.ox.ac.uk>
2026-07-29 16:41:10 +0100

    Update dev proxy to match new tileserver routes

--- frontend/dev-proxy/proxy-table.dev.js
+++ frontend/dev-proxy/proxy-table.dev.js
@@ -1,13 +1,11 @@
 export const devProxy = {
-  '/raster': {
-    target: 'http://localhost:5001',
+  '/tiles/raster': {
+    target: 'http://localhost:8000',
     changeOrigin: true,
-    rewrite: (path) => path.replace(/^\/raster/, ''),
   },
-  '/vector': {
-    target: 'http://localhost:8800',
+  '/tiles/vector': {
+    target: 'http://localhost:8000',
     changeOrigin: true,
-    rewrite: (path) => path.replace(/^\/vector/, ''),
   },
   '/api': {
     target: 'http://localhost:8000',
